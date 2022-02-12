88ae7004fd4b0eb628a70f425961192d149102c4
Billy Doyle <doyle.william98@gmail.com>
2022-02-12 16:30:50 -0500

    global entry script with notification for nix

---
+++ global_entry.py
@@ -0,0 +1,59 @@
+import time
+import subprocess
+import requests
+
+def fetch(location_id):
+
+    # 5444 is good to test
+    # JFK IS 5140
+
+    agent = (
+        "Mozilla/5.0 (X11; Linux x86_64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/85.0.4183.102 Safari/537.36"
+    )
+    params = {
+        'orderBy': 'soonest',
+        'limit': 1,
+        'locationId': location_id,
+        'minimum': 1
+    }
+    url = 'https://ttp.cbp.dhs.gov/schedulerapi/slots'
+    r = requests.get(url, params=params, headers={'user-agent': agent})
+    if not r.ok:
+        r.raise_for_status()
+
+    return r.json()
+
+def main(location_id):
+
+    set_ = set()
+    ct = 0
+    while True:
+        ct += 1
+        if ct % 100 == 0:
+            print(ct, time.ctime())
+
+        try:
+            resp = fetch(location_id)
+        except requests.RequestException as e:
+            print(f'Exception at {time.ctime()}, sleeping...')
+            time.sleep(60)
+
+        if len(resp) == 0:
+            time.sleep(5)
+        else:
+            message = 'Global Entry Appt @ ' + resp[0]['startTimestamp']
+            if message not in set_:
+                print(message)
+                subprocess.Popen(['notify-send', message])
+                set_.add(message)
+
+            time.sleep(5)
+
+
+if __name__ == '__main__':
+    # JFK 5140
+    # LAX 2720
+    # see airports.json for other locations
+    main(5140)

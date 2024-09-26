4a598f68c2e91f620643b651ababd02be88a1c06
Billy Doyle <50842911+billy-doyle@users.noreply.github.com>
2024-09-26 18:16:32 -0400

    feat/uv (#5)
    update doc
    
    update doc

--- global_entry.py
+++ global_entry.py
@@ -1,61 +1,100 @@
-import sys
-import os
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "plyer>2",
+#     "pyobjus>=1.2.3",
+#     "requests<3",
+# ]
+# ///
+import argparse
+import platform
+import subprocess
 import time
+
 import requests
 from plyer import notification
+from requests.adapters import HTTPAdapter, Retry
 
-def fetch(location_id):
 
+class CustomHTTPAdapter(HTTPAdapter):
+    def __init__(self, timeout=None, *args, **kwargs):
+        self.timeout = timeout
+        super().__init__(*args, **kwargs)
+
+    def send(self, *args, **kwargs):
+        kwargs["timeout"] = self.timeout
+        return super().send(*args, **kwargs)
+
+
+def create_session(
+    location_id,
+    timeout=10,
+    num_retries=5,
+    backoff_factor=0.1,
+    status_forcelist=(404, 500, 502, 503, 504),
+):
+    session = requests.Session()
     agent = (
         "Mozilla/5.0 (X11; Linux x86_64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/85.0.4183.102 Safari/537.36"
     )
-    params = {
-        'orderBy': 'soonest',
-        'limit': 1,
-        'locationId': location_id,
-        'minimum': 1
-    }
-    url = 'https://ttp.cbp.dhs.gov/schedulerapi/slots'
-    r = requests.get(url, params=params, headers={'user-agent': agent})
+    session.headers.update({"User-Agent": agent})
+    params = {"orderBy": "soonest", "limit": 1, "locationId": location_id, "minimum": 1}
+    session.params.update(params)
+
+    retries = Retry(
+        total=num_retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+
+    session.mount("http://", CustomHTTPAdapter(timeout=timeout, max_retries=retries))
+    session.mount("https://", CustomHTTPAdapter(timeout=timeout, max_retries=retries))
+
+    return session
+
+
+def notify(message):
+    if platform.system() == "Darwin":
+        subprocess.run(
+            [f'osascript -e \'display notification "{message}" with title "global_entry.py"\''],
+            shell=True,
+        )
+    else:
+        notification.notify(title="global_entry.py", message=message, timeout=3)
+
+
+def fetch(session):
+    url = "https://ttp.cbp.dhs.gov/schedulerapi/slots"
+    r = session.get(url)
     if not r.ok:
         r.raise_for_status()
 
     return r.json()
 
+
 def main(location_id):
-
     set_ = set()
-    ct = 0
+    session = create_session(location_id)
     while True:
-        ct += 1
-        if ct % 100 == 0:
-            print(ct, time.ctime())
-
-        try:
-            resp = fetch(location_id)
-        except requests.RequestException as e:
-            print(f'Exception at {time.ctime()}, sleeping...')
-            time.sleep(60)
-
-        if len(resp) == 0:
-            time.sleep(5)
-        else:
-            message = 'Global Entry Appt @ ' + resp[0]['startTimestamp']
+        resp = fetch(session)
+        if len(resp) != 0:
+            message = "Global Entry Appt @ " + resp[0]["startTimestamp"]
             if message not in set_:
                 print(message)
-                try:
-                    notification.notify(title='global_entry.py', message=message, timeout=3)
-                except:
-                    os.system("osascript -e 'display notification \"{}\"\'".format(message))
+                notify(message)
                 set_.add(message)
 
-            time.sleep(5)
+        time.sleep(5)
 
 
-if __name__ == '__main__':
-    # JFK 5140, LAX 2720, default to JFK
-    # see airports.json for other locations
-    default = 5140 if len(sys.argv) == 1 else sys.argv[1]
-    main(default)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="default to JFK, see airports.json for other locations (LAX 2720, SFO 5446)"
+    )
+    parser.add_argument(
+        "location_id", nargs="?", default=5140, help="An optional integer value", type=int
+    )
+    args = parser.parse_args()
+    main(args.location_id)
